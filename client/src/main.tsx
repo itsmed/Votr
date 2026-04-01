8bc3d539dbd961d4b2bf92209600c0c43abc528f
Dejon Gill <dejonjgill@gmail.com>
2026-04-01 17:16:39 -0400

    configure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { configure } from '@pollus/shared';
 import '../app/globals.css';
 import App from './App';
+
+configure({ apiUrl: import.meta.env.VITE_API_URL ?? 'http://localhost:4000' });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
